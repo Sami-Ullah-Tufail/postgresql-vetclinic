bc357886a07a7b9c1233a5c790778361decf1ae2
Sami-Ullah-Tufail <sami.u.tufail@gmail.com>
2023-11-04 01:52:47 +0500

    changing BIT to boolean

--- data.sql
+++ data.sql
@@ -6,7 +6,7 @@ INSERT INTO animals (
     escape_attempts,    
     neutered,    
     weight_kg
-    )VALUES ('1', 'Agumon', '2020-02-3', 0,'1', 10.23),  
-    ('2', 'Gabumon', '2018-11-15', 2,'1', 8.0),  
-    ('3', 'Pikachu', '2021-01-07', 1, '0', 15.04),  
-    ('4', 'Devimon', '2017-05-12', 5, '1', 11.0);
+    )VALUES ('1', 'Agumon', '2020-02-3', 0,true, 10.23),  
+    ('2', 'Gabumon', '2018-11-15', 2,true, 8.0),  
+    ('3', 'Pikachu', '2021-01-07', 1, false'0', 15.04),  
+    ('4', 'Devimon', '2017-05-12', 5, true, 11.0);
