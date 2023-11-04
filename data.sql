013a7c1494ad649649c7754354b4949279295221
Sami-Ullah-Tufail <sami.u.tufail@gmail.com>
2023-11-04 16:32:49 +0500

    day 2 update

--- data.sql
+++ data.sql
@@ -10,3 +10,20 @@ INSERT INTO animals (
     ('2', 'Gabumon', '2018-11-15', 2,true, 8.0),  
     ('3', 'Pikachu', '2021-01-07', 1, false'0', 15.04),  
     ('4', 'Devimon', '2017-05-12', 5, true, 11.0);
+
+    -- day 2 data input 
+    INSERT INTO animals (
+     id,
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+VALUES ( '5', 'Charmander', '2020-02-08', -11,  '0', 0),
+  ( '6', 'Plantmon', '2021-11-15', -5.7,  '1', 2),
+  ('7', 'Squirtle', '1993-04-02', -12.13, '0', 3),
+  ('8', 'Angemon', '2005-06-12', -45,  '1', 1),
+  ('9', 'Boarmon', '2005-06-07', 20.4, '1', 7),
+  ('10', 'Blossom', '1998-10-13', 17,  '1', 3),
+  ('11', 'Ditto', '2022-05-14', 22,  '1', 4);
